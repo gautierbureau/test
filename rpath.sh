4679ff6c814791200e8222e0e8426d00e1a7b122
Gautier Bureau <gautier.bureau@rte-france.com>
2025-11-19 14:24:05 +0100

    test

--- rpath.sh
+++ rpath.sh
@@ -12,3 +12,9 @@ for file in `find "$install_path"/lib -type f -name "lib*.dylib"`; do
     install_name_tool -change $lib_path @rpath/$(echo $lib_path | awk -F'/' '{print $(NF)}') $file
   done
 done
+
+for file in `find "$install_path"/ddb -type f -name "lib*.dylib"`; do
+  for lib_path in $(otool -l $file | grep -A2 LC_LOAD_DYLIB | grep dylib | grep name | awk '{print $2}' | grep -v "@.*path" | grep -v "^/usr/lib/" | grep -v "^/usr/local/lib/" | grep -v "^/System"); do
+    install_name_tool -change $lib_path @rpath/$(echo $lib_path | awk -F'/' '{print $(NF)}') $file
+  done
+done
