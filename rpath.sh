7aacc0650fd96be96701158def61e7d5a8c3560e
Gautier Bureau <gautier.bureau@rte-france.com>
2025-11-19 14:41:22 +0100

    test

--- rpath.sh
+++ rpath.sh
@@ -15,7 +15,15 @@ for file in `find "$install_path"/lib -type f -name "lib*.dylib"`; do
   done
 done
 
-for file in `find "$install_path"/ddb -type f -name "lib*.dylib"`; do
+for file in `find "$install_path"/lib -type f -name "dynawo_*.dylib"`; do
+  echo $file
+  for lib_path in $(otool -l $file | grep -A2 LC_LOAD_DYLIB | grep dylib | grep name | awk '{print $2}' | grep -v "@.*path" | grep -v "^/usr/lib/" | grep -v "^/usr/local/lib/" | grep -v "^/System"); do
+    echo "   $lib_path"
+    install_name_tool -change $lib_path @rpath/$(echo $lib_path | awk -F'/' '{print $(NF)}') $file
+  done
+done
+
+for file in `find "$install_path"/ddb -type f -name "dynawo_*.dylib"`; do
   for lib_path in $(otool -l $file | grep -A2 LC_LOAD_DYLIB | grep dylib | grep name | awk '{print $2}' | grep -v "@.*path" | grep -v "^/usr/lib/" | grep -v "^/usr/local/lib/" | grep -v "^/System"); do
     install_name_tool -change $lib_path @rpath/$(echo $lib_path | awk -F'/' '{print $(NF)}') $file
   done
