8186ca1a0b37140f0bbbcfb9e30d56fd222a55f1
Gautier Bureau <gautier.bureau@rte-france.com>
2025-11-19 14:20:58 +0100

    test

---
+++ rpath.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "You need to give the path for install."
+  exit 1
+fi
+
+install_path="$1"
+
+for file in `find "$install_path"/lib -type f -name "lib*.dylib"`; do
+  for lib_path in $(otool -l $file | grep -A2 LC_LOAD_DYLIB | grep dylib | grep name | awk '{print $2}' | grep -v "@.*path" | grep -v "^/usr/lib/" | grep -v "^/usr/local/lib/" | grep -v "^/System"); do
+    install_name_tool -change $lib_path @rpath/$(echo $lib_path | awk -F'/' '{print $(NF)}') $file
+  done
+done
